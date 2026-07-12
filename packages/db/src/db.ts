1cf7a42663f14e24de49b7873c74eac8705a116d
Raman-Khiva <ramandeep01167@gmail.com>
2026-07-12 15:40:26 +0000

    fix db import issue

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,12 +1,13 @@
-import { PrismaClient } from "../src/generated/client.ts";
+import { PrismaClient } from "../src/generated/client"
 // Import the driver adapter for your specific database (example uses PostgreSQL)
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg"
 
 // Initialize the adapter according to your driver's requirements
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 // Pass the adapter instance to PrismaClient
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter })
 
 // Find all users with their posts
-export default prisma;
+export default prisma
+
